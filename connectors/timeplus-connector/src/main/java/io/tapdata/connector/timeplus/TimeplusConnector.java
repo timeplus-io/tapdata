f7cd2ef2e0d4fa0dcea0c6f5317662035260e468
Jove Zhong <jove@timeplus.io>
2023-01-16 11:29:38 -0800

    code skeleton for timeplus sink

---
+++ connectors/timeplus-connector/src/main/java/io/tapdata/connector/timeplus/TimeplusConnector.java
@@ -0,0 +1,81 @@
+package io.tapdata.connector.timeplus;
+
+import io.tapdata.base.ConnectorBase;
+import io.tapdata.entity.codec.TapCodecsRegistry;
+import io.tapdata.entity.event.dml.TapDeleteRecordEvent;
+import io.tapdata.entity.event.dml.TapInsertRecordEvent;
+import io.tapdata.entity.event.dml.TapRecordEvent;
+import io.tapdata.entity.event.dml.TapUpdateRecordEvent;
+import io.tapdata.entity.logger.TapLogger;
+import io.tapdata.entity.schema.TapTable;
+import io.tapdata.entity.utils.DataMap;
+import io.tapdata.pdk.apis.annotations.TapConnectorClass;
+import io.tapdata.pdk.apis.context.TapConnectionContext;
+import io.tapdata.pdk.apis.context.TapConnectorContext;
+import io.tapdata.pdk.apis.entity.ConnectionOptions;
+import io.tapdata.pdk.apis.entity.TestItem;
+import io.tapdata.pdk.apis.entity.WriteListResult;
+import io.tapdata.pdk.apis.functions.ConnectorFunctions;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Consumer;
+
+@TapConnectorClass("spec_timeplus.json")
+public class TimeplusConnector extends ConnectorBase {
+
+    @Override
+    public void onStart(TapConnectionContext connectionContext) throws Throwable {
+
+    }
+
+    @Override
+    public void onStop(TapConnectionContext connectionContext) throws Throwable {
+
+    }
+
+    @Override
+    public void registerCapabilities(ConnectorFunctions connectorFunctions, TapCodecsRegistry codecRegistry) {
+
+    }
+
+    @Override
+    public void discoverSchema(TapConnectionContext connectionContext, List<String> tables, int tableSize, Consumer<List<TapTable>> consumer) throws Throwable {
+
+    }
+
+    @Override
+    public ConnectionOptions connectionTest(TapConnectionContext connectionContext, Consumer<TestItem> consumer) throws Throwable {
+        return null;
+    }
+
+    @Override
+    public int tableCount(TapConnectionContext connectionContext) throws Throwable {
+        return 0;
+    }
+
+    private void writeRecord(TapConnectorContext connectorContext, List<TapRecordEvent> tapRecordEvents, TapTable table, Consumer<WriteListResult<TapRecordEvent>> writeListResultConsumer) {
+        //TODO write records into database
+        //Below is sample code to print received events which suppose to write to database.
+        AtomicLong inserted = new AtomicLong(0); //insert count
+        AtomicLong updated = new AtomicLong(0); //update count
+        AtomicLong deleted = new AtomicLong(0); //delete count
+        for(TapRecordEvent recordEvent : tapRecordEvents) {
+            if(recordEvent instanceof TapInsertRecordEvent) {
+                inserted.incrementAndGet();
+                //TapLogger.info(TAG, "Record Write TapInsertRecordEvent {}", toJson(recordEvent));
+            } else if(recordEvent instanceof TapUpdateRecordEvent) {
+                updated.incrementAndGet();
+                //TapLogger.info(TAG, "Record Write TapUpdateRecordEvent {}", toJson(recordEvent));
+            } else if(recordEvent instanceof TapDeleteRecordEvent) {
+                deleted.incrementAndGet();
+                //TapLogger.info(TAG, "Record Write TapDeleteRecordEvent {}", toJson(recordEvent));
+            }
+        }
+        //Need to tell incremental engine the write result
+        writeListResultConsumer.accept(writeListResult()
+                .insertedCount(inserted.get())
+                .modifiedCount(updated.get())
+                .removedCount(deleted.get()));
+    }
+}
